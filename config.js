f64c1bb2cdc3b4d8f1b3324ff398538bd14676c5
Nandakishor Nair <nandakishorsj@gmail.com>
2021-12-27 17:44:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: 'Alternative Treatment Protocols for Covid-19',
+    description: 'Full stack dev education',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/'},
+            { text: 'Guide', link: '/guide/'},
+            { text: 'About us', link: 'https://awakenindiamovement.com/'},
+        ],
+        sidebar: {
+            '/guide/': [
+                '',
+                'introduction',
+                'prepandprinci',
+                'intprophyproto',
+                'intearlycurative',
+                'intapplongcovid',
+                'intappvaxrecovery',
+                'naturopathic',
+                'homeopathic',
+                'allopathic',
+                'ayurvedic',
+                'siddha',
+                'tcm'
+            ]
+        }
+    }
+}
